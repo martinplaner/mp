4b1c5859504bc18ad029fd2bb51e33583520b993
Martin Planer <martin.planer@gmail.com>
2020-08-25 19:10:38 +0200

    Load template from http.Filesystem

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
-//go:generate go run assets_generate.go assets.go
+//go:generate go run generate.go assets.go templates.go
 
 import (
 	"context"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -35,52 +36,74 @@ func main() {
 		logger.Fatal("failed to load config", zap.Error(err))
 	}
 
-	logger.Info("configuration",
-		zap.String("file", config.File),
-		zap.String("listen", config.Listen),
+	logger.Info("effective configuration",
+		zap.Reflect("config", config),
 	)
 
-	if !config.Debug {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	router := gin.New()
-
-	ginLogger := logger.Named("gin")
-	router.Use(ginzap.Ginzap(ginLogger, time.RFC3339, true))
-	router.Use(ginzap.RecoveryWithZap(ginLogger, true))
-	router.LoadHTMLGlob("templates/*.tmpl")
+	router := configureGin(config)
 
 	generator, err := GeneratorFromFile(config.File, "-")
 	if err != nil {
 		logger.Fatal("failed to create generator", zap.Error(err))
 	}
 	router.GET("/", handlerWithGenerator(generator))
-	router.StaticFS("/_assets", assets)
+	router.StaticFS("/_assets/", assets)
 
 	srv := &http.Server{
+		// TODO handle timeouts?
 		Addr:    config.Listen,
 		Handler: router,
 	}
 
-	go runServer(srv)
-	waitForSignal()
-	shutdown(srv)
+	runServer(srv)
+}
+
+func configureGin(config *Config) *gin.Engine {
+	if !config.Debug {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.Default()
+
+	t, err := loadTemplate("index.tmpl")
+	if err != nil {
+		logger.Fatal("failed to load templates", zap.Error(err))
+	}
+	router.SetHTMLTemplate(t)
+
+	return router
+}
+
+func loadTemplate(name string) (*template.Template, error) {
+	file, err := templates.Open(name)
+	if err != nil {
+		logger.Fatal("failed to load template", zap.String("name", name), zap.Error(err))
+	}
+	defer file.Close()
+
+	h, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	t, err := template.New(name).Parse(string(h))
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
 }
 
 func runServer(srv *http.Server) {
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatal("listen failed", zap.Error(err))
-	}
-}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal("listen failed", zap.Error(err))
+		}
+	}()
 
-func waitForSignal() {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-}
 
-func shutdown(srv *http.Server) {
 	logger.Info("server shutting down...")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
