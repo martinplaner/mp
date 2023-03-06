872c4e32cc79e2963d2e3d2dccabe9b68601875b
Martin Planer <martin.planer@gmail.com>
2023-03-06 19:41:50 +0100

    Add new config options

--- config.go
+++ config.go
@@ -4,17 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 // Config holds the global program configuration (parsed from command line flags and environment variables).
 type Config struct {
-	Debug  bool
-	File   string
-	Listen string
-	Files  *InputFiles
+	Debug        bool
+	DefaultQuery string
+	File         string
+	Listen       string
+	Mode         Mode
+	Once         string
 }
 
+//go:generate stringer -type Mode -output config_string.go
+
+// Mode defines the word generation mode.
+// Either compound nouns (default) or a single noun prefixed by a list of adjectives.
 type Mode int64
 
 const (
@@ -22,60 +27,17 @@ const (
 	Adjective Mode = 1
 )
 
-func (m Mode) String() string {
-	switch m {
-	case Compound:
-		return "c"
-	case Adjective:
-		return "a"
+func (m Mode) Set(value string) error {
+	switch value {
+	case "a", "adjective":
+		m = Adjective
+		return nil
+	case "c", "compound":
+		m = Compound
+		return nil
 	}
 
-	panic("unexpected mode")
-}
-
-var modeMap = map[string]Mode{
-	"c": Compound,
-	"a": Adjective,
-}
-
-type InputFile struct {
-	Language string
-	Mode     Mode
-	Filename string
-}
-
-type InputFiles []*InputFile
-
-func (i *InputFiles) String() string {
-	var b strings.Builder
-	for _, f := range *i {
-		b.WriteString(f.Language)
-		b.WriteString(":")
-		b.WriteString(f.Mode.String())
-		b.WriteString(":")
-		b.WriteString(f.Filename)
-		b.WriteString(";")
-	}
-	return b.String()
-}
-
-func (i *InputFiles) Set(value string) error {
-	var langArg string
-	var mode string
-	var filename string
-
-	_, err := fmt.Sscanf(value, "%s:%s:%s", &langArg, &mode, &filename)
-	if err != nil {
-		return fmt.Errorf("could not parse file argument: %w", err)
-	}
-
-	*i = append(*i, &InputFile{
-		Language: langArg,
-		Mode:     Compound,
-		Filename: filename,
-	})
-
-	return nil
+	return fmt.Errorf("invalid mode value: %v", value)
 }
 
 func loadConfig() (*Config, error) {
@@ -84,14 +46,14 @@ func loadConfig() (*Config, error) {
 		port = "8080"
 	}
 
-	c := &Config{
-		Files: &InputFiles{},
-	}
+	c := &Config{}
 
 	flag.BoolVar(&c.Debug, "debug", false, "Enable verbose debug mode")
-	// flag.StringVar(&c.File, "file", "de:c:words_de.txt", "Path to word list (one word per line)")
+	flag.StringVar(&c.DefaultQuery, "default", "MP", "Default fallback query term, if not provided.")
+	flag.StringVar(&c.File, "file", "words.txt", "Path to word list (one word per line, with optional word type)")
 	flag.StringVar(&c.Listen, "listen", fmt.Sprintf(":%s", port), "TCP address for the server to listen on, in the form 'host:port'")
-	flag.Var(c.Files, "file", "TODO write input file usage")
+	flag.Var(&c.Mode, "mode", "Word generation mode, one of a|adjective|c|compound")
+	flag.StringVar(&c.Once, "once", "", "Run generation once with the given query and print result, then quit")
 	flag.Parse()
 
 	return c, nil
